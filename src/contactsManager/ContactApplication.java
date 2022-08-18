e26baa39b6f787c9ae56c9c4ad74b5cf7246a48c
Darien Salazar <darien.axell.salazar@gmail.com>
2022-08-18 11:35:56 -0500

    Added menu and switch case

--- src/contactsManager/ContactApplication.java
+++ src/contactsManager/ContactApplication.java
@@ -1,7 +1,55 @@
 package contactsManager;
 
-public class ContactApplication {
-    public static void main(String[] args) {
+import util.Input;
 
+public class ContactApplication {
+    public static final int INVALID_CHOICE = -1;
+    public static final int EXIT_CHOICE = 0;
+
+    public static void main(String[] args) {
+        Input input = new Input();
+        // loop until the user says he/she does not wish to continue
+        int choice = INVALID_CHOICE;
+        while (choice != EXIT_CHOICE) {
+            printMenu();
+            choice = input.getInt(0, 6, "");
+            doChoice(choice);
+        }
+    }
+
+    private static void printMenu() {
+        System.out.print("""
+                    
+                1 - View contacts.
+                2 - Add a new contact.
+                3 - Search a contact by name.
+                4 - Delete an existing contact.
+                5 - Exit.
+                            
+                Enter an option:\040""");
+    }
+
+    private static void doChoice(int choice) {
+        // TODO: call your functions based on whatever the user's choice is
+        switch (choice) {
+            case 1:
+                // TODO: view all contacts
+                break;
+            case 2:
+                // TODO: add a new contact
+                break;
+            case 3:
+                // Search contact by name
+                break;
+            case 4:
+                // TODO: Delete contact
+                break;
+            case 5:
+                // TODO: Exit
+                break;
+
+
+        }
     }
 }
+
---
+++ src/contactsManager/Contacts.java
@@ -0,0 +1,13 @@
+package contactsManager;
+
+public class Contacts {
+    private String firstName;
+    private String lastName;
+    private int phoneNumber;
+
+    public Contacts(String firstName, String lastName, int phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+}
