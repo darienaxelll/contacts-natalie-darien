107e1f89b9fc97a5dd2c99f816368a8c3c18af1c
NatalieSneed <106278703+NatalieSneed@users.noreply.github.com>
2022-08-19 12:19:25 -0500

    add menu options

--- src/contactsManager/ContactList.java
+++ src/contactsManager/ContactList.java
@@ -23,10 +23,7 @@ public class ContactList {
         }
     }
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 52e883ae9d1de1051161e629ff69c3f9d44d3953
     private int getIndexOfItemByName(String itemName) {
         int index = -1;
         for (int i = 0; i < contactItems.size(); i++) {
